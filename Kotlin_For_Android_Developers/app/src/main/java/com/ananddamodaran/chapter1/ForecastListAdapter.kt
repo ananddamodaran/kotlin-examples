13fd11114f9b84ffa8a0245abebdc2a3a5cb870f
Anand Damodaran <dvlpr.anand@gmail.com>
2018-03-12 07:21:53 +0530

    classes and functions

---
+++ Kotlin_For_Android_Developers/app/src/main/java/com/ananddamodaran/chapter1/ForecastListAdapter.kt
@@ -0,0 +1,25 @@
+package com.ananddamodaran.chapter1
+
+import android.support.v7.widget.RecyclerView
+import android.view.ViewGroup
+import android.widget.TextView
+
+/**
+ * Created by anand on 3/12/18.
+ */
+class ForecastListAdapter(val items: List<String>) :
+        RecyclerView.Adapter<ForecastListAdapter.ViewHolder>(){
+    override fun getItemCount(): Int = items.size
+
+    override fun onCreateViewHolder(parent: ViewGroup?, viewType: Int): ViewHolder {
+        val textView = TextView(parent?.context)
+        return  ViewHolder(textView)
+    }
+
+    override fun onBindViewHolder(holder: ViewHolder?, position: Int) {
+        holder?.textView?.text = items[position]
+    }
+
+    class ViewHolder(val textView:TextView) : RecyclerView.ViewHolder(textView)
+
+}
--- kotlin_for_android_developers.md
+++ kotlin_for_android_developers.md
@@ -15,3 +15,6 @@
  	* default value in parameters which prevents the need of function overloading
 	* named arguments can be used to write the name of the argument preceding the value to specify which one you want.
 - String templates - ${user.name}
+- Property naming
+- New keyword is not required to instantiate an object
+- 

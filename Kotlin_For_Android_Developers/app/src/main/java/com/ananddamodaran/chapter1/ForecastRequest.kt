574de108cf7ad56d2791e742c46e99c351bd2855
Anand Damodaran <dvlpr.anand@gmail.com>
2018-03-19 20:16:22 +0530

    app config updated

--- Kotlin_For_Android_Developers/app/src/main/java/com/ananddamodaran/chapter1/ForecastRequest.kt
+++ Kotlin_For_Android_Developers/app/src/main/java/com/ananddamodaran/chapter1/ForecastRequest.kt
@@ -1,5 +1,6 @@
 package com.ananddamodaran.chapter1
 
+import android.util.Log
 import com.ananddamodaran.chapter1.data.ForeCastResult
 import com.google.gson.Gson
 import java.net.URL
@@ -7,15 +8,23 @@ import java.net.URL
 /**
  * Created by anand on 3/15/18.
  */
-class ForecastRequest(val zipCode: String){
+class ForecastRequest(val zipCode: String) {
     companion object {
-        private val APP_ID="57235b43518963f7ee1d6fcf4c8da75e"
-        private val WEATHER_URL="http://api.openweathermap.org/data/2.5/weather?" +
+        //  private val APP_ID="57235b43518963f7ee1d6fcf4c8da75e"
+        private val APP_ID = "15646a06818f61f7b8d7823ca833e1ce"
+
+        private val WEATHER_URL = "http://api.openweathermap.org/data/2.5/weather?" +
                 "appid=$APP_ID&q="
+        private val URL = "http://api.openweathermap.org/data/2.5/forecast/daily?mode=json&units=metric&cnt=7"
+        private val COMPLETE_URL = "$URL&APPID=$APP_ID&q="
+
+
     }
-    fun execute():ForeCastResult{
-        val forecastJsonString = URL(WEATHER_URL+zipCode).readText()
-        return Gson().fromJson(forecastJsonString,ForeCastResult::class.java)
+
+    fun execute(): ForeCastResult {
+        val forecastJsonString = URL(COMPLETE_URL + zipCode).readText()
+        Log.d("ForecastRequest", forecastJsonString)
+        return Gson().fromJson(forecastJsonString, ForeCastResult::class.java)
 
     }
 }
